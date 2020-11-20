5a4edada1893c4636eacb4293e6648c8cc3c37cf
Ronaldo B <ronny@localhost.localdomain>
2020-11-20 02:40:52 -0400

    Se incorporo libreria intervention\image para optimizar tamanios de imagenes y aniadir marca de agua, se creo seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+//use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        DB::table('users')->insert([
+            'name' => 'Administrador',
+            'email' => 'administradorjcitunari@gmail.com',
+            'password' => Hash::make('admin#123&'),
+        ]);
     }
 }
